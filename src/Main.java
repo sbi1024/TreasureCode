f62e567d2c4623a9c95b087dfeea53ac3c494c94
seoboin <sbi1024@naver.com>
2022-12-18 22:08:46 +0900

    master commit git add .

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("git branch name is subDevelop");
         System.out.println("git branch name is Develop");
-
+        System.out.println("git branch name is master commit and push !!!");
     }
 }

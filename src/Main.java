f69b3a99eeb8ff2427927dee60e9045a224d5e7b
seoboin <sbi1024@naver.com>
2022-12-18 22:24:02 +0900

    last commit Test git add .

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("git branch name is subDevelop");
         System.out.println("git branch name is Develop");
+        System.out.println("args = " + args);
+        if(true){
+            System.out.println("args = " + Arrays.toString(args));
+        }else{
+            System.out.println("git last Test !!");
+        }
+
 
     }
 }

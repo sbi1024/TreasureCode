52e598a5ba507cca70bbeaf3b446c2e4ab569d3f
seoboin <sbi1024@naver.com>
2022-12-18 22:01:41 +0900

    develop write code git add .

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("git branch name is subDevelop");
+        System.out.println("git branch name is Develop");
+
     }
 }
